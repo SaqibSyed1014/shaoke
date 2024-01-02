ad47084d9153185a93f67ee41f87cb0137dfbf04
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-01-03 02:32:21 +0500

    google maps integrated

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 const app = createApp(App)
 
-app.use(router)
+app
+    .use(router)
+    .use(VueGoogleMaps, {
+        load: {
+            key: 'AIzaSyCEmD75xZcZ5L72rrw1RFtwcY9HXsW7JCI',
+        },
+    })
 
 app.mount('#app')
